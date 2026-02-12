687f21ca56edcc55711901868efd405ddbfec546
Amit Devrani <amitdevraniad@gmail.com>
2026-02-12 22:38:42 +0530

    content fixed

--- app/research/[slug]/page.tsx
+++ app/research/[slug]/page.tsx
@@ -55,10 +55,7 @@ export default function TransmissionPostPage() {
       try {
         const response = await fetch(
           `https://news.sanpec-excellence.com/wp-json/wp/v2/posts?slug=${slug}&_embed`,
-            {
-            next: { revalidate: 300 }, // ✅ 5 minutes cache
-            cache: 'force-cache' // ✅ Force cache
-          }
+          
         );
         
         if (!response.ok) {
